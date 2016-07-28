df96040401232299222dec303ba84cb5fb54342f
izubkov <izybkov@gmail.com>
2016-07-28 15:55:02 -0700

    basic div-percent-distrib

--- src/fin-forecasting.clj
+++ src/fin-forecasting.clj
@@ -18,7 +18,40 @@
   (:use [anglican core emit runtime
          [inference :refer [infer equalize]]]))
 ;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
+;; <=
 
+;; @@
+(defm log-normal
+  (normal mean std))
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;financial-forecasting/log-normal</span>","value":"#'financial-forecasting/log-normal"}
+;; <=
+
+;; @@
+(defm div-percent-distrib [Ft]
+  (> (:volume Ft) 0.5))
+    
 ;; @@
 
 ;; @@
+(defm at-given-dt-Ft [Ft dt div-percent-distrib]
+  (if dt
+    (let [div-percent (sample div-percent-distrib)]
+      (log-normal
+        (* div-percent (:earnings Ft) 1))
+      (dirac 0))
+    )
+  )
+;; @@
+
+;; @@
+(doquery :smc
+  at-given-dt-Ft [xval]
+  :number-of-particles 100)
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;financial-forecasting/div-percent-distrib</span>","value":"#'financial-forecasting/div-percent-distrib"}
+;; <=
