48f75eb11aae5a2ac02edfdec662da93df616195
izubkov <izybkov@gmail.com>
2016-07-28 11:34:45 -0700

    initial Anglican worksheet

---
+++ src/fin-forecasting.clj
@@ -0,0 +1,24 @@
+;; gorilla-repl.fileformat = 1
+
+;; **
+;;; # Gorilla REPL
+;;; 
+;;; Welcome to gorilla :-)
+;;; 
+;;; Shift + enter evaluates code. Hit alt+g twice in quick succession or click the menu icon (upper-right corner) for more commands ...
+;;; 
+;;; It's a good habit to run each worksheet in its own namespace: feel free to use the declaration we've provided below if you'd like.
+;; **
+
+;; @@
+(ns financial-forecasting
+  (:require [gorilla-plot.core :as plot]
+            [anglican lmh]
+            [anglican.stat :as stat])
+  (:use [anglican core emit runtime
+         [inference :refer [infer equalize]]]))
+;; @@
+
+;; @@
+
+;; @@
