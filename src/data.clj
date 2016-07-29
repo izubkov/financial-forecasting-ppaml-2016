e2cd8b944ab9e4869f9badb781671b5d70609678
izubkov <izybkov@gmail.com>
2016-07-29 11:38:48 -0700

    data as vector

--- src/data.clj
+++ src/data.clj
@@ -1,9 +1,9 @@
-(def Ft-data {:'30-06-2013' {:net-income 0.35 :div 0.5625 :spike 0 :stock-price 58.35 :volume 60551066}
-              :'30-09-2013' {:net-income 0.98 :div 0.5625 :spike 1 :stock-price 60.046 :volume 57507000}
-              :'31-03-2014' {:net-income 0.65 :div 0.6000 :spike 0 :stock-price 70.97 :volume 50194333}
-              :'30-06-2014' {:net-income 0.27 :div 0.6000 :spike 0 :stock-price 71.0 :volume 49018766}
-              :'30-09-2014' {:net-income 0.90 :div 0.6000 :spike 1 :stock-price 68.98 :volume 45221900}
-              :'31-03-2015' {:net-income 0.91 :div 0.6500 :spike 0 :stock-price 71.48 :volume 63794600}
-              :'30-06-2015' {:net-income 0.70 :div 0.6475 :spike 0 :stock-price 69.69 :volume 52825033}
-              :'30-09-2015' {:net-income 1.00 :div 0.6475 :spike 1 :stock-price 70.61 :volume 56544466}
-              :'30-03-2016' {:net-income 0.88 :div 0.7000 :spike 0 :stock-price 72.52 :volume 69868350}})
+(def Ft-data (vector {:date "30-06-2013" :net-income 0.35 :div 0.5625 :spike 0 :stock-price 58.35  :volume 60551066}
+                     {:date "30-09-2013" :net-income 0.98 :div 0.5625 :spike 1 :stock-price 60.046 :volume 57507000}
+                     {:date "31-03-2014" :net-income 0.65 :div 0.6000 :spike 0 :stock-price 70.97  :volume 50194333}
+                     {:date "30-06-2014" :net-income 0.27 :div 0.6000 :spike 0 :stock-price 71.0   :volume 49018766}
+                     {:date "30-09-2014" :net-income 0.90 :div 0.6000 :spike 1 :stock-price 68.98  :volume 45221900}
+                     {:date "31-03-2015" :net-income 0.91 :div 0.6500 :spike 0 :stock-price 71.48  :volume 63794600}
+                     {:date "30-06-2015" :net-income 0.70 :div 0.6475 :spike 0 :stock-price 69.69  :volume 52825033}
+                     {:date "30-09-2015" :net-income 1.00 :div 0.6475 :spike 1 :stock-price 70.61  :volume 56544466}
+                     {:date "30-03-2016" :net-income 0.88 :div 0.7000 :spike 0 :stock-price 72.52  :volume 69868350}))
