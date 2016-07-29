fb5ebf8b00f0f1a1f2c3e4bf792e97e38450a403
izubkov <izybkov@gmail.com>
2016-07-29 14:39:13 -0700

    some debugging

--- src/fin-forecasting.clj
+++ src/fin-forecasting.clj
@@ -50,23 +50,23 @@
 (defquery at-given-dt-Ft [Ft]
   ; This is a model (simulator) for amount of dividents for next quarter
   (let [ ; we do not know our distrib. yes - we'll learn it
-         do-inc-distrib     (sample (uniform-continuous 0. 1.))
-         do-not-inc-distrib (sample (uniform-continuous 0. 1.))]
+         do-inc-distrib     (sample (normal 0. 1.))
+         do-not-inc-distrib (sample (normal 0. 1.))]
     (loop [fi (first Ft) ; first quarter
            fts (rest Ft)
-           model-param-inc   do-inc-distrib ; initial inc distrib.
-           model-param-noinc do-not-inc-distrib] ; initial no-inc distrib.
+           m-inc    do-inc-distrib ; initial inc distrib.
+           m-no-inc do-not-inc-distrib] ; initial no-inc distrib.
       (if fts
         ((let [addition-distrib
                (amount-to-increase-div-distrib
-                 (div-increase-boolean fi do-inc-distrib do-not-inc-distrib)
+                 (div-increase-boolean fi m-inc m-no-inc)
                  0.05 0.1)]
-           (observe (addition-distrib) do-inc-distrib)
-           (observe (addition-distrib) do-not-inc-distrib))
-         (recur (first fts) (rest fts) do-inc-distrib do-not-inc-distrib))
-        ({} () do-inc-distrib do-not-inc-distrib)))
-    {:do-inc-distrib     do-inc-distrib
-     :do-not-inc-distrib do-not-inc-distrib}))
+           (observe (addition-distrib) m-inc)
+           (observe (addition-distrib) m-no-inc))
+         (recur (first fts) (rest fts) m-inc m-no-inc))
+        ({} () m-inc m-no-inc)))
+    {:do-inc-distrib     m-inc
+     :do-not-inc-distrib m-no-inc}))
 ;;(loop [futurequarters []]
 ;;  .... with sample)}))
 ;; @@
